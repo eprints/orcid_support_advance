1f19250ca2ef94a36c0a08d87fe3807a10f1192e
Will Fyson <williamfyson89@hotmail.com>
2018-08-01 08:49:18 +0000

    Remove debugging and update emp/empi.

--- lib/plugins/EPrints/Plugin/Event/OrcidSync.pm
+++ lib/plugins/EPrints/Plugin/Event/OrcidSync.pm
@@ -46,11 +46,8 @@ sub update_employment
                 {
 			my $orgid1 = $institution->{'organization'}->{'disambiguated-organization'}->{'disambiguated-organization-identifier'};
         		my $orgid2 = $employment->{'organization'}->{'disambiguated-organization'}->{'disambiguated-organization-identifier'};
-			print STDERR "id1.....$orgid1\n";
-			print STDERR "id2.....$orgid2\n";
 		        if( $orgid1 eq $orgid2 )
 			{
-				print STDERR "don't add institution\n";
 		                $add_institution = 0;
 		        }
                 }
